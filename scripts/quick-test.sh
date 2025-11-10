5508fb28ac475ea346f1d1997c4a3888cb5a67c0
JamesT <james@cg-8663.com>
2025-11-11 01:19:07 +0800

    Prepare repository for initial public release

--- scripts/quick-test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Quick test suite for ShadowMesh
-set -e
-
-echo "Running quick tests..."
-go test ./shared/crypto -short
-go test ./shared/protocol -short  
-go test ./test/integration -short
-echo "All tests passed!"
